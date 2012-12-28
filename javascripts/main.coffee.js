86352bac302a1f2a199cdb0cda5abad001af9967
Wangwei <wei1.wang@dinglicom.com>
2012-12-28 15:13:53 +0800

    this for test.

--- javascripts/main.coffee.js
+++ javascripts/main.coffee.js
@@ -1,7 +1,7 @@
 (function() {
 
   $(document).ready(function() {
-    return $('ol').after('<h2>你好，Coffee Script is ready, :-D </h2>');
+    return $('ol').after('<h2>你好，Nanana。Coffee Script is ready, :-D </h2>');
   });
 
 }).call(this);

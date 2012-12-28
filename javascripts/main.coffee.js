d2859dfb54da57a62b6b98509e83e83d12c165be
Wangwei <wei1.wang@dinglicom.com>
2012-12-28 14:32:19 +0800

    this commit from test.

--- javascripts/main.coffee.js
+++ javascripts/main.coffee.js
@@ -1,7 +1,7 @@
 (function() {
 
   $(document).ready(function() {
-    return $('ol').after('<h2>你好，Coffee Script!(Added from coffee/main.coffee)</h2>');
+    return $('ol').after('<h2>你好，Coffee Script is ready, :-D </h2>');
   });
 
 }).call(this);

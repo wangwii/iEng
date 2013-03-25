65a525666e9f5c51a74df0c160bd339799ffa84c
Wangwei <wei1.wang@dinglicom.com>
2013-03-25 23:08:26 +0800

    fixed Chinese problem

--- javascripts/main.coffee.js
+++ javascripts/main.coffee.js
@@ -1,7 +1,26 @@
 (function() {
+  var Phonetic;
 
   $(document).ready(function() {
-    return $('ol').after('<h2>你好，Nanana。Coffee Script is ready, :-D </h2>');
+    return $('ol').after('<h2>Nanana 娜娜，你好吗？ 理论为核心那你干嘛那.Coffee Script is ready, :-D </h2>');
   });
+
+  Phonetic = (function() {
+
+    Phonetic.create = function(params) {
+      return alert(params);
+    };
+
+    function Phonetic(x) {}
+
+    Phonetic.prototype.id = void 0;
+
+    Phonetic.prototype.name = '';
+
+    Phonetic.prototype.voice = 'http://voice.url/code';
+
+    return Phonetic;
+
+  })();
 
 }).call(this);

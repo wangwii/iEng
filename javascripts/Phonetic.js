74b753ed8b3fc21686fd7fb13c1af7161297c052
Wangwei <wei1.wang@dinglicom.com>
2013-05-30 20:32:09 +0800

    modify the phonetic.js file

--- javascripts/Phonetic.js
+++ javascripts/Phonetic.js
@@ -1,9 +1,14 @@
 var Phonetic = {
-  iː: {},
-
+  iː: {
+    abc: ["e", "ee", "ea", "ei", "ie"],
+    words: ["he", "see", "heat", "ceiling", "field"],
+    type: "vowel",
+    group: "1"
+  },
   i: {
-    type: 'yuanyin_short',
-    words: []
-
-  } ə ɔ u ʌ e æ
+    abc: ["i", "ui", "y"],
+    words: ["fit", "build", "myth"],
+    type: "vowel",
+    group: "1"
+  }// ə ɔ u ʌ e æ
 };

a1898fb3a0b76fbc320faf1594c394ee8d9aa0a4
Wangwei <wei1.wang@dinglicom.com>
2013-06-02 18:08:09 +0800

    added the bootstrap and the phonetic.

---
+++ phonetic/Phonetic.js
@@ -0,0 +1,28 @@
+var Phonetic = {
+  // ə ɔ u ʌ
+  iː: {
+    abc: ["e", "ee", "ea", "ei", "ie"],
+    words: ["he", "see", "heat", "ceiling", "field"],
+    type: "vowel",
+    group: "1"
+  },
+  i: {
+    abc: ["i", "ui", "y"],
+    words: ["fit", "build", "myth"],
+    type: "vowel",
+    group: "1"
+  },
+  e: {
+    abc: ["e", "ea"],
+    words: ["bed", "dest", "head", "bread"],
+    type: "vowel",
+    group: "2"
+  },
+  æ: {
+    abc: ["a"],
+    words: ["map","stamp","hat"],
+    type: "vowel",
+    group: "2"
+  }
+
+};
